3b9c2e717a61fef0844a126d602adf392b8949f0
ae <tgioer@163.com>
2019-08-26 23:51:17 +0800

    Allow all methods.

--- shop-catalog-project/shop-catalog-spring-boot/src/main/java/com/github/shop/WebMvcConfiguration.java
+++ shop-catalog-project/shop-catalog-spring-boot/src/main/java/com/github/shop/WebMvcConfiguration.java
@@ -28,6 +28,6 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
     }
 }
