a2df08ed1ac6c12a6f34c4d05c13d789f98734fc
ae <tgioer@163.com>
2019-09-19 11:03:04 +0800

    Remove '@EnableWebMvc' annotation.

--- shop-catalog-project/shop-catalog-spring-boot/src/main/java/com/github/shop/WebMvcConfiguration.java
+++ shop-catalog-project/shop-catalog-spring-boot/src/main/java/com/github/shop/WebMvcConfiguration.java
@@ -18,10 +18,8 @@ package com.github.shop;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
