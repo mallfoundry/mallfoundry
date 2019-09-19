fb45b4f458be266e3fa217f34f58a859de6cc4f8
ae <tgioer@163.com>
2019-09-19 11:08:19 +0800

    Create the StorageSystem object if the StorageSystem class exists.

--- shop-common-project/shop-autoconfigure/src/main/java/com/github/shop/spring/boot/autoconfigure/storage/ShopStorageAutoConfiguration.java
+++ shop-common-project/shop-autoconfigure/src/main/java/com/github/shop/spring/boot/autoconfigure/storage/ShopStorageAutoConfiguration.java
@@ -20,6 +20,7 @@ import com.github.shop.storage.StorageConfiguration;
 import com.github.shop.storage.StorageSystem;
 import com.github.shop.storage.StorageSystems;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -33,6 +34,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ShopStorageAutoConfiguration implements WebMvcConfigurer {
 
     @Bean
+    @ConditionalOnClass(StorageSystem.class)
     public StorageSystem storageSystem(ShopStorageProperties properties) {
         return StorageSystems.newStorageSystem(this.getConfiguration(properties));
     }
