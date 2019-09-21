51f8b1366425d4a125d7a496c5591f3c8d70cb55
ae <tgioer@163.com>
2019-09-21 16:50:33 +0800

    Add 'TopicIdentifier' class.

---
+++ shop-basic-project/shop-topic/src/main/java/com/github/shop/topic/TopicIdentifier.java
@@ -0,0 +1,31 @@
+package com.github.shop.topic;
+
+import java.util.UUID;
+
+/**
+ * Topic object identifier.
+ */
+public class TopicIdentifier {
+
+    /**
+     * Generate a new comment id.
+     *
+     * @return comment id
+     */
+    public static String newCommentId() {
+        return uuid();
+    }
+
+    /**
+     * Generate a new reply id.
+     *
+     * @return comment id
+     */
+    public static String newReplyId() {
+        return uuid();
+    }
+
+    private static String uuid() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+}
