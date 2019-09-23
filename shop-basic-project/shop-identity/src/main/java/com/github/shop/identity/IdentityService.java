d245be295a1a00868f66752e320d05a15fac5f62
ae <tgioer@163.com>
2019-09-23 18:53:32 +0800

    Add identity service interface.

---
+++ shop-basic-project/shop-identity/src/main/java/com/github/shop/identity/IdentityService.java
@@ -0,0 +1,30 @@
+package com.github.shop.identity;
+
+/**
+ * @author Zhi Tang
+ */
+public interface IdentityService {
+
+    /**
+     * Create a user.
+     *
+     * @param user a user
+     */
+    void createUser(User user);
+
+    /**
+     * Delete the user based on the username.
+     *
+     * @param username username
+     */
+    void deleteUser(String username);
+
+    /**
+     * Update user information based on username
+     *
+     * @param user a user
+     */
+    void updateUser(User user);
+
+    User getUser(String username);
+}
