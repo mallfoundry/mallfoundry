8173dc4d8f8a18f46d53cd0e2bc0c1ca41d0f530
ae <tgioer@163.com>
2019-09-25 15:53:26 +0800

    Add a mybatis implementation of PurchaseOrderRepository.

---
+++ shop-buyer-project/shop-buyer-internal/src/main/java/com/github/shop/buyer/infrastructure/persistence/mybatis/PurchaseOrderMapper.java
@@ -0,0 +1,21 @@
+package com.github.shop.buyer.infrastructure.persistence.mybatis;
+
+import com.github.shop.buyer.PurchaseOrder;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface PurchaseOrderMapper {
+
+    void insertOrder(PurchaseOrder order);
+
+    void updateOrder(PurchaseOrder order);
+
+    void deleteOrderById(@Param("id") String id);
+
+    List<PurchaseOrder> selectListByCart(@Param("cart") String cart);
+}
