1cd1491258508cace2a79f3d44af0c5b1119becc
ae <tgioer@163.com>
2019-09-25 16:18:16 +0800

    Update the buyer rest resource interface.

--- shop-buyer-project/shop-buyer-rest/src/main/java/com/github/shop/buyer/rest/BuyerResourceV1.java
+++ shop-buyer-project/shop-buyer-rest/src/main/java/com/github/shop/buyer/rest/BuyerResourceV1.java
@@ -2,10 +2,16 @@ package com.github.shop.buyer.rest;
 
 import com.github.shop.buyer.PurchaseOrder;
 import com.github.shop.buyer.ShoppingCartService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/buyer")
@@ -17,8 +23,31 @@ public class BuyerResourceV1 {
         this.shoppingCartService = shoppingCartService;
     }
 
-    @PostMapping("/purchase/orders")
-    public void addOrder(@RequestBody PurchaseOrder order) {
+    @PostMapping("/shopping_carts/{cart}/orders")
+    public void addOrder(@PathVariable("cart") String cart,
+                         @RequestBody PurchaseOrder order) {
+        order.setCart(cart);
         this.shoppingCartService.addOrder(order);
+    }
+
+    @DeleteMapping("/shopping_carts/{cart}/orders/{order_id}")
+    public void removeOrder(@PathVariable("cart") String cart,
+                            @PathVariable("order_id") String orderId) {
+        PurchaseOrder order = new PurchaseOrder();
+        order.setCart(cart);
+        order.setId(orderId);
+        this.shoppingCartService.removeOrder(order);
+    }
+
+    @PatchMapping("/shopping_carts/{cart}/orders")
+    public void updateOrder(@PathVariable("cart") String cart,
+                            @RequestBody PurchaseOrder order) {
+        order.setCart(cart);
+        this.shoppingCartService.updateOrder(order);
+    }
+
+    @GetMapping("/shopping_carts/{cart}/orders")
+    public List<PurchaseOrder> getOrders(@PathVariable("cart") String cart) {
+        return this.shoppingCartService.getOrders(cart);
     }
 }
