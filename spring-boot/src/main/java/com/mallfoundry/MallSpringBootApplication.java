0dd64f5b9f2044d10fc5331073c7990d945aebae
ae <tgioer@163.com>
2019-12-18 22:17:45 +0800

    Use jpa instead of mybatis.

--- spring-boot/src/main/java/com/mallfoundry/MallSpringBootApplication.java
+++ spring-boot/src/main/java/com/mallfoundry/MallSpringBootApplication.java
@@ -18,8 +18,10 @@ package com.mallfoundry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableJpaRepositories
 @EnableTransactionManagement
 @SpringBootApplication
 public class MallSpringBootApplication {
