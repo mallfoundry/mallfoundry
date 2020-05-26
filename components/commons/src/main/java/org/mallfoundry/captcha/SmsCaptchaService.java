faf0183c8d4ea5b464c28062f410bf3006f148b3
TangZhi <tgioer@163.com>
2020-05-26 17:04:07 +0800

    Commit codes.

--- components/commons/src/main/java/org/mallfoundry/captcha/SmsCaptchaService.java
+++ components/commons/src/main/java/org/mallfoundry/captcha/SmsCaptchaService.java
@@ -4,5 +4,15 @@ package org.mallfoundry.captcha;
 // mall.captcha.type=sms
 // mall.captcha.sms.template=xxx
 // mall.captcha.sms.signature=xxx
-public class SmsCaptchaService {
+public class SmsCaptchaService implements CaptchaService {
+
+    @Override
+    public Captcha createCaptcha(CaptchaType type) {
+        return null;
+    }
+
+    @Override
+    public boolean checkCaptcha(Captcha captcha) {
+        return false;
+    }
 }
