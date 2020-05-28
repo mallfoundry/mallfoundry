7200555a34d0d9536acff35a5c753fb500d1502c
TangZhi <tgioer@163.com>
2020-05-28 15:34:55 +0800

    Commit codes.

--- components/catalog-rest/src/main/java/org/mallfoundry/rest/catalog/BrandResourceV1.java
+++ components/catalog-rest/src/main/java/org/mallfoundry/rest/catalog/BrandResourceV1.java
@@ -1,12 +1,19 @@
 package org.mallfoundry.rest.catalog;
 
+import org.mallfoundry.catalog.Brand;
 import org.mallfoundry.catalog.BrandService;
+import org.mallfoundry.data.SliceList;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/v1")
@@ -24,13 +31,21 @@ public class BrandResourceV1 {
                 this.brandService.addBrand(request.assignToBrand(this.brandService.createBrand(null))));
     }
 
+    @GetMapping("/brands")
+    public SliceList<Brand> getBrands(@RequestParam(name = "page", defaultValue = "1") Integer page,
+                                      @RequestParam(name = "limit", defaultValue = "100") Integer limit,
+                                      @RequestParam(name = "categories", required = false) Set<String> categories) {
+        return this.brandService.getBrands(
+                this.brandService.createBrandQuery().toBuilder().page(page).limit(limit).categories(categories).build());
+    }
+
     @PatchMapping("/brands/{brand_id}")
     public BrandResponse saveBrand(@PathVariable("brand_id") String brandId, @RequestBody BrandRequest request) {
         return new BrandResponse(
                 this.brandService.saveBrand(request.assignToBrand(this.brandService.getBrand(brandId).orElseThrow())));
     }
 
-    @PatchMapping("/brands/{brand_id}")
+    @DeleteMapping("/brands/{brand_id}")
     public void deleteBrand(@PathVariable("brand_id") String brandId) {
         this.brandService.deleteBrand(brandId);
     }
