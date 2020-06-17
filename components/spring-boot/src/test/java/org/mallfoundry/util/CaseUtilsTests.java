dc31200d0d312e7611efd1a16c3b3ff648131dcc
TangZhi <tgioer@163.com>
2020-06-17 18:51:11 +0800

    添加CaseUtils。

---
+++ components/spring-boot/src/test/java/org/mallfoundry/util/CaseUtilsTests.java
@@ -0,0 +1,8 @@
+package org.mallfoundry.util;
+
+public class CaseUtilsTests {
+
+    public static void main(String[] args) {
+        System.out.println(CaseUtils.camelCase("da_dasf_da"));
+    }
+}
