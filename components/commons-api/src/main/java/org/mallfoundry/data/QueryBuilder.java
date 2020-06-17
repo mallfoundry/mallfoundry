705a6dc42dc5626138a1752a7a748a31bebef306
TangZhi <tgioer@163.com>
2020-06-17 18:56:56 +0800

    添加Query接口，使其支持排序功能。

--- components/commons-api/src/main/java/org/mallfoundry/data/QueryBuilder.java
+++ components/commons-api/src/main/java/org/mallfoundry/data/QueryBuilder.java
@@ -4,5 +4,5 @@ import java.util.function.Function;
 
 public interface QueryBuilder<O extends Query, B extends QueryBuilder<O, B>> extends PageableBuilder<O, B> {
 
-    QueryBuilder<O, B> sort(Function<O, Sort> function);
+    B sort(Function<Sort, Sort> function);
 }
--- components/commons-api/src/main/java/org/mallfoundry/data/QueryBuilderSupport.java
+++ components/commons-api/src/main/java/org/mallfoundry/data/QueryBuilderSupport.java
@@ -3,18 +3,21 @@ package org.mallfoundry.data;
 import java.util.function.Function;
 
 public abstract class QueryBuilderSupport<O extends Query, B extends QueryBuilder<O, B>>
+        extends PageableBuilderSupport<O, B>
         implements QueryBuilder<O, B> {
 
     private final O query;
 
     public QueryBuilderSupport(O query) {
+        super(query);
         this.query = query;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    public QueryBuilder<O, B> sort(Function<O, Sort> function) {
-        this.query.setSort(function.apply(this.query));
-        return this;
+    public B sort(Function<Sort, Sort> function) {
+        this.query.setSort(function.apply(new DefaultSort()));
+        return (B) this;
     }
 }
 
