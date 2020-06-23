80cb7a62376d079ae3c15f9604de8dacb28c12b5
TangZhi <tgioer@163.com>
2020-06-23 11:12:08 +0800

    添加@Repository注解。

--- components/storage/src/main/java/org/mallfoundry/storage/repository/jpa/JpaIndexBlobRepository.java
+++ components/storage/src/main/java/org/mallfoundry/storage/repository/jpa/JpaIndexBlobRepository.java
@@ -6,9 +6,11 @@ import org.mallfoundry.storage.IndexBlobRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface JpaIndexBlobRepository
         extends IndexBlobRepository, JpaRepository<IndexBlob, IndexBlobId> {
 
