37eeebe5cc8a1489f530e4d5475d42cc064c1b39
TangZhi <tgioer@163.com>
2020-06-28 12:53:47 +0800

    完善阿里云上传服务。

--- components/storage/src/main/java/org/mallfoundry/storage/aliyun/AliyunStorageSystem.java
+++ components/storage/src/main/java/org/mallfoundry/storage/aliyun/AliyunStorageSystem.java
@@ -2,32 +2,25 @@ package org.mallfoundry.storage.aliyun;
 
 import com.aliyun.oss.OSS;
 import org.apache.commons.lang3.StringUtils;
+import org.mallfoundry.storage.AbstractStorageSystem;
 import org.mallfoundry.storage.Blob;
 import org.mallfoundry.storage.StorageException;
-import org.mallfoundry.storage.StoragePathReplacer;
-import org.mallfoundry.storage.StorageSystem;
 import org.mallfoundry.util.PathUtils;
 import org.springframework.beans.factory.DisposableBean;
 
 import java.io.IOException;
 import java.util.Objects;
-import java.util.Optional;
 
-public class AliyunStorageSystem implements StorageSystem, DisposableBean {
-
-    private final StoragePathReplacer pathReplacer;
+public class AliyunStorageSystem extends AbstractStorageSystem implements DisposableBean {
 
     private final OSS client;
 
     private String bucketName;
 
-    private String baseUrl;
-
-    public AliyunStorageSystem(OSS client, String bucketName, String baseUrl, StoragePathReplacer pathReplacer) {
+    public AliyunStorageSystem(OSS client, String bucketName, String baseUrl) {
+        super(baseUrl);
         this.client = client;
-        this.pathReplacer = pathReplacer;
         this.setBucketName(bucketName);
-        this.setBaseUrl(baseUrl);
     }
 
     public void setBucketName(String bucketName) {
@@ -37,42 +30,14 @@ public class AliyunStorageSystem implements StorageSystem, DisposableBean {
         this.bucketName = StringUtils.trim(bucketName);
     }
 
-    public void setBaseUrl(String baseUrl) {
-        if (StringUtils.isBlank(baseUrl)) {
-            throw new StorageException("The base url must not be empty");
-        }
-        this.baseUrl = Optional.of(baseUrl)
-                .map(String::trim)
-                .map(s -> s.endsWith("/")
-                        ? s
-                        : s + "/")
-                .orElseThrow();
-    }
-
     @Override
-    public void storeBlob(Blob blob) throws IOException {
+    public void storeBlobToPath(Blob blob, String path) throws IOException {
         if (blob.isFile()) {
             var file = blob.toFile();
-            var filePath = this.pathReplacer.replace(file);
-            this.client.putObject(this.bucketName, this.removePrefixSeparator(filePath), file);
-            blob.setUrl(this.concatUrl(filePath));
+            this.client.putObject(this.bucketName, PathUtils.removePrefixSeparator(path), file);
+            blob.setUrl(this.concatAccessUrl(path));
             blob.setSize(file.length());
         }
-    }
-
-    private String concatUrl(String path) {
-        var url = path.startsWith("/")
-                ? path.substring(1)
-                : path;
-        return String.join("", this.baseUrl, url);
-    }
-
-    private String removePrefixSeparator(String path) {
-        return Optional.of(path).map(PathUtils::normalize)
-                .map(s -> s.startsWith("/")
-                        ? s.substring(1)
-                        : s)
-                .orElseThrow();
     }
 
     @Override
