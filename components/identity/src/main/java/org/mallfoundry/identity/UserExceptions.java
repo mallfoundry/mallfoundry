e95c2322e52d775e3ee575560b8ed791a7ab1608
TangZhi <tgioer@163.com>
2020-06-30 12:30:03 +0800

    设置为工具类。

--- components/identity/src/main/java/org/mallfoundry/identity/UserExceptions.java
+++ components/identity/src/main/java/org/mallfoundry/identity/UserExceptions.java
@@ -2,7 +2,7 @@ package org.mallfoundry.identity;
 
 import static org.mallfoundry.i18n.MessageHolder.message;
 
-public class UserExceptions {
+public abstract class UserExceptions {
 
     public static UserException notExists(String user) {
         return new UserException(
