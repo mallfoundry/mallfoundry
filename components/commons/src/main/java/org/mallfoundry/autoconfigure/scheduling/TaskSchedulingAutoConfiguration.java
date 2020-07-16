688adec5c6e20bbd1a58cd883bf679224fd78539
TangZhi <tgioer@163.com>
2020-07-16 18:46:13 +0800

    创建`ThreadPoolTaskScheduler`对象。

---
+++ components/commons/src/main/java/org/mallfoundry/autoconfigure/scheduling/TaskSchedulingAutoConfiguration.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.autoconfigure.scheduling;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.boot.task.TaskSchedulerBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+@ConditionalOnClass(ThreadPoolTaskScheduler.class)
+@Configuration(proxyBeanMethods = false)
+@AutoConfigureAfter({TaskExecutionAutoConfiguration.class})
+public class TaskSchedulingAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean({TaskScheduler.class, ScheduledExecutorService.class})
+    public ThreadPoolTaskScheduler taskScheduler(TaskSchedulerBuilder builder) {
+        return builder.build();
+    }
+}
