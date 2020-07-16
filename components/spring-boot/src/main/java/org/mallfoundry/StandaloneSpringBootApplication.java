3610501da8976c6ca35a7ed9bbf03167b8625844
TangZhi <tgioer@163.com>
2020-07-16 18:49:34 +0800

    添加`@EnableAsync`和`@EnableScheduling`配置。

--- components/spring-boot/src/main/java/org/mallfoundry/StandaloneSpringBootApplication.java
+++ components/spring-boot/src/main/java/org/mallfoundry/StandaloneSpringBootApplication.java
@@ -22,8 +22,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableAsync
+@EnableScheduling
 @EnableCaching
 @EnableJpaRepositories
 @EnableTransactionManagement
