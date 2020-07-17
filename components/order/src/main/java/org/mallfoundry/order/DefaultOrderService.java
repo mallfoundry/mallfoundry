db357b90a010f0ccfea1aa54ceac88df54df2952
TangZhi <tgioer@163.com>
2020-07-17 15:20:08 +0800

    使用订单处理器优化订单业务。

--- components/order/src/main/java/org/mallfoundry/order/DefaultOrderService.java
+++ components/order/src/main/java/org/mallfoundry/order/DefaultOrderService.java
@@ -20,26 +20,17 @@ package org.mallfoundry.order;
 
 import org.apache.commons.lang3.StringUtils;
 import org.mallfoundry.data.SliceList;
-import org.mallfoundry.keygen.PrimaryKeyHolder;
 import org.mallfoundry.security.SubjectHolder;
 import org.mallfoundry.shipping.CarrierService;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.data.util.CastUtils;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.Set;
+import java.util.function.Function;
 
 public class DefaultOrderService implements OrderService {
-
-    static final String ORDER_ID_VALUE_NAME = "order.id";
-
-    static final String ORDER_ITEM_ID_VALUE_NAME = "order.item.id";
-
-    static final String ORDER_SHIPMENT_ID_VALUE_NAME = "order.shipment.id";
-
-    static final String ORDER_SHIPMENT_ITEM_ID_VALUE_NAME = "order.shipment.item.id";
 
     private final OrderConfiguration orderConfiguration;
 
@@ -88,28 +79,43 @@ public class DefaultOrderService implements OrderService {
         return this.placeOrders(List.of(order));
     }
 
+    private <T, R> Function<List<T>, List<T>> publishEvent(Function<T, R> function) {
+        return (List<T> tt) -> {
+            tt.stream().map(function).forEach(this.eventPublisher::publishEvent);
+            return tt;
+        };
+    }
+
+    private List<Order> invokePlaceOrders(List<Order> orders) {
+        orders.forEach(order -> order.place(this.orderConfiguration.getDefaultExpires()));
+        return orders;
+    }
+
     @Transactional
     @Override
     public List<Order> placeOrders(List<Order> orders) {
-        var placingOrders = this.orderSplitter.splitting(orders).stream()
-                .peek(order -> order.setId(PrimaryKeyHolder.next(ORDER_ID_VALUE_NAME)))
-                .peek(order -> order.getItems().forEach(item -> item.setId(PrimaryKeyHolder.next(ORDER_ITEM_ID_VALUE_NAME))))
-                .peek(order -> order.place(this.orderConfiguration.getDefaultExpires()))
-                .collect(Collectors.toList());
-        List<Order> placedOrders = this.orderRepository.saveAll(placingOrders);
-        placedOrders.forEach(order ->
-                this.eventPublisher.publishEvent(new ImmutableOrderPlacedEvent(order)));
-        return placedOrders;
+        return Function.<List<Order>>identity()
+                .compose(this.publishEvent(ImmutableOrderPlacedEvent::new))
+                .compose(this.orderRepository::saveAll)
+                .compose(this.processorsInvoker::invokePreProcessPlaceOrders)
+                .compose(this::invokePlaceOrders)
+                .<List<Order>>compose(this.orderSplitter::splitting)
+                .apply(orders);
     }
 
     @Override
     public Optional<Order> getOrder(String orderId) {
-        return this.orderRepository.findById(orderId).map(this.processorsInvoker::invokeProcessPostGetOrder);
+        return this.orderRepository.findById(orderId)
+                .map(this.processorsInvoker::invokePostProcessGetOrder);
     }
 
     @Override
     public SliceList<Order> getOrders(OrderQuery query) {
-        return CastUtils.cast(this.orderRepository.findAll(query));
+        return Function.<SliceList<Order>>identity()
+                .compose(this.processorsInvoker::invokePostProcessGetOrders)
+                .compose(this.orderRepository::findAll)
+                .compose(this.processorsInvoker::invokePreProcessGetOrders)
+                .apply(query);
     }
 
     @Override
@@ -120,57 +126,68 @@ public class DefaultOrderService implements OrderService {
     @Transactional
     @Override
     public Order updateOrder(Order order) {
-        return this.orderRepository.save(order);
+        return Function.<Order>identity()
+                .compose(this.orderRepository::save)
+                .compose(this.processorsInvoker::invokePreProcessUpdateOrder)
+                .apply(order);
     }
 
-    private Order getNonOrder(String orderId) {
-        return this.orderRepository.findById(orderId).orElseThrow(OrderExceptions::notFound);
+    private Order requiredOrder(String orderId) {
+        return this.orderRepository.findById(orderId)
+                .orElseThrow(OrderExceptions::notFound);
     }
 
     @Transactional
     @Override
     public void payOrder(String orderId, PaymentInformation details) {
-        var order = getNonOrder(orderId);
+        var order = this.requiredOrder(orderId);
         order.pay(details);
         this.eventPublisher.publishEvent(new ImmutableOrderPaidEvent(order));
     }
 
     @Transactional
     @Override
-    public void signOrder(String orderId) {
-        var order = getNonOrder(orderId);
-        order.sign();
+    public void fulfilOrder(String orderId) {
+        var order = this.processorsInvoker.invokePreProcessFulfilOrder(requiredOrder(orderId));
+        order.fulfil();
+        var savedOrder = this.orderRepository.save(order);
+        this.eventPublisher.publishEvent(new ImmutableOrderFulfilledEvent(savedOrder));
+    }
+
+    @Transactional
+    @Override
+    public void signOrder(String orderId, String message) {
+        var order = this.requiredOrder(orderId);
+        message = this.processorsInvoker.invokePreProcessSignOrder(order, message);
+        order.sign(message);
+        var savedOrder = this.orderRepository.save(order);
+        this.eventPublisher.publishEvent(new ImmutableOrderSignedEvent(savedOrder));
     }
 
     @Transactional
     @Override
     public void receiptOrder(String orderId) {
-        var order = getNonOrder(orderId);
+        var order = this.processorsInvoker.invokePreProcessReceiptOrder(this.requiredOrder(orderId));
         order.receipt();
+        var savedOrder = this.orderRepository.save(order);
+        this.eventPublisher.publishEvent(new ImmutableOrderReceivedEvent(savedOrder));
     }
 
     @Transactional
     @Override
     public void cancelOrder(String orderId, String reason) {
-        var order = getNonOrder(orderId);
+        var order = requiredOrder(orderId);
+        reason = this.processorsInvoker.invokePreProcessCancelOrder(order, reason);
         order.cancel(reason);
-    }
-
-    @Transactional
-    @Override
-    public void fulfilOrder(String orderId) {
-        var order = getNonOrder(orderId);
-        order.fulfil();
+        var savedOrder = this.orderRepository.save(order);
+        this.eventPublisher.publishEvent(new ImmutableOrderCancelledEvent(savedOrder));
     }
 
     @Transactional
     @Override
     public Shipment addOrderShipment(String orderId, Shipment shipment) {
-        var order = this.getOrder(orderId).orElseThrow();
-        if (StringUtils.isBlank(shipment.getId())) {
-            shipment.setId(PrimaryKeyHolder.next(ORDER_SHIPMENT_ID_VALUE_NAME));
-        }
-        shipment.getItems().forEach(item -> item.setId(PrimaryKeyHolder.next(ORDER_SHIPMENT_ITEM_ID_VALUE_NAME)));
+        var order = this.requiredOrder(orderId);
+        shipment = this.processorsInvoker.invokePreProcessAddOrderShipment(order, shipment);
         shipment.setConsignorId(SubjectHolder.getUserId());
         if (StringUtils.isBlank(shipment.getConsignor())) {
             shipment.setConsignor(SubjectHolder.getNickname());
@@ -187,18 +204,27 @@ public class DefaultOrderService implements OrderService {
 
     @Override
     public Optional<Shipment> getOrderShipment(String orderId, String shipmentId) {
-        return this.orderRepository.findById(orderId).orElseThrow().getShipment(shipmentId);
+        return Function.<Optional<Shipment>>identity()
+                .<Order>compose(order -> order.getShipment(shipmentId))
+                .compose(this.processorsInvoker::invokePreProcessGetOrderShipment)
+                .compose(this::requiredOrder)
+                .apply(orderId);
     }
 
     @Override
     public List<Shipment> getOrderShipments(String orderId) {
-        return this.orderRepository.findById(orderId).orElseThrow().getShipments();
+        return Function.<List<Shipment>>identity()
+                .<Order>compose(Order::getShipments)
+                .compose(this.processorsInvoker::invokePreProcessGetOrderShipments)
+                .compose(this::requiredOrder)
+                .apply(orderId);
     }
 
     @Transactional
     @Override
     public void updateOrderShipment(String orderId, Shipment shipment) {
-        var order = this.orderRepository.findById(orderId).orElseThrow();
+        var order = this.requiredOrder(orderId);
+        shipment = this.processorsInvoker.invokePreProcessUpdateOrderShipment(order, shipment);
         order.updateShipment(shipment);
         this.orderRepository.save(order);
     }
@@ -206,14 +232,31 @@ public class DefaultOrderService implements OrderService {
     @Transactional
     @Override
     public void updateOrderShipments(String orderId, List<Shipment> shipments) {
-        var order = this.orderRepository.findById(orderId).orElseThrow();
-        shipments.forEach(order::updateShipment);
+        var order = this.requiredOrder(orderId);
+        shipments = this.processorsInvoker.invokePreProcessUpdateOrderShipments(order, shipments);
+        order.updateShipments(shipments);
+        this.orderRepository.save(order);
+    }
+
+    private Shipment requiredOrderShipment(Order order, String shipmentId) {
+        return order.getShipment(shipmentId).orElseThrow();
     }
 
     @Transactional
     @Override
     public void removeOrderShipment(String orderId, String shipmentId) {
-        var order = this.orderRepository.findById(orderId).orElseThrow();
-        order.removeShipment(order.getShipment(shipmentId).orElseThrow());
+        var order = this.requiredOrder(orderId);
+        var shipment = this.processorsInvoker.invokePreProcessRemoveOrderShipment(order, this.requiredOrderShipment(order, shipmentId));
+        order.removeShipment(shipment);
+        this.orderRepository.save(order);
+    }
+
+    @Transactional
+    @Override
+    public void removeOrderShipments(String orderId, Set<String> shipmentIds) {
+        var order = this.requiredOrder(orderId);
+        var shipments = this.processorsInvoker.invokePreProcessRemoveOrderShipments(order, order.getShipments(shipmentIds));
+        order.removeShipments(shipments);
+        this.orderRepository.save(order);
     }
 }
