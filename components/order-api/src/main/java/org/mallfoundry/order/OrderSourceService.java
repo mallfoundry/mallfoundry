c058676c64a2d88c0f873b4d27d1377ce9f00738
TangZhi <tgioer@163.com>
2020-07-19 18:44:14 +0800

    添加`getSourceLabel`方法。

--- components/order-api/src/main/java/org/mallfoundry/order/OrderSourceService.java
+++ components/order-api/src/main/java/org/mallfoundry/order/OrderSourceService.java
@@ -16,7 +16,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-package org.mallfoundry.shipping;
+package org.mallfoundry.order;
 
-public interface Parcel {
+public interface OrderSourceService {
+
+    String getSourceLabel(OrderSource source);
 }
