9aaa1de125e0f81055788002774ff3015b3fd959
TangZhi <tgioer@163.com>
2020-07-23 14:42:58 +0800

    删除无用构造器。

--- components/keystone-api/src/main/java/org/mallfoundry/security/acl/AccessDeniedException.java
+++ components/keystone-api/src/main/java/org/mallfoundry/security/acl/AccessDeniedException.java
@@ -21,16 +21,7 @@ package org.mallfoundry.security.acl;
 import org.mallfoundry.security.SecurityException;
 
 public class AccessDeniedException extends SecurityException {
-
     public AccessDeniedException(String message) {
         super(message);
-    }
-
-    public AccessDeniedException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public AccessDeniedException(Throwable cause) {
-        super(cause);
     }
 }
