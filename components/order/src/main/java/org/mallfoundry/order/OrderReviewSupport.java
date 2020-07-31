355ad6d128656bf18f793eab07c3e1a83454eeab
TangZhi <tgioer@163.com>
2020-07-31 10:19:56 +0800

    添加`Builder`实现。

--- components/order/src/main/java/org/mallfoundry/order/OrderReviewSupport.java
+++ components/order/src/main/java/org/mallfoundry/order/OrderReviewSupport.java
@@ -19,6 +19,11 @@
 package org.mallfoundry.order;
 
 import java.util.Date;
+import java.util.List;
+
+import static org.mallfoundry.order.OrderReviewStatus.APPROVED;
+import static org.mallfoundry.order.OrderReviewStatus.DISAPPROVED;
+import static org.mallfoundry.order.OrderReviewStatus.PENDING;
 
 public abstract class OrderReviewSupport implements MutableOrderReview {
 
@@ -29,16 +34,125 @@ public abstract class OrderReviewSupport implements MutableOrderReview {
 
     @Override
     public void create() {
+        this.setStatus(PENDING);
         this.setCreatedTime(new Date());
     }
 
     @Override
     public void approve() {
+        this.setStatus(APPROVED);
         this.setReviewedTime(new Date());
     }
 
     @Override
     public void disapprove() {
+        this.setStatus(DISAPPROVED);
         this.setReviewedTime(new Date());
+    }
+
+    @Override
+    public Builder toBuilder() {
+        return new BuilderSupport(this) {
+        };
+    }
+
+    protected abstract static class BuilderSupport implements Builder {
+        private final OrderReviewSupport review;
+
+        protected BuilderSupport(OrderReviewSupport review) {
+            this.review = review;
+        }
+
+        @Override
+        public Builder id(String id) {
+            this.review.setId(id);
+            return this;
+        }
+
+        @Override
+        public Builder itemId(String itemId) {
+            this.review.setItemId(itemId);
+            return this;
+        }
+
+        @Override
+        public Builder productId(String productId) {
+            this.review.setProductId(productId);
+            return this;
+        }
+
+        @Override
+        public Builder variantId(String variantId) {
+            this.review.setVariantId(variantId);
+            return this;
+        }
+
+        @Override
+        public Builder rating(int rating) {
+            this.review.setRating(rating);
+            return this;
+        }
+
+        @Override
+        public Builder anonymous() {
+            this.review.anonymous();
+            return this;
+        }
+
+        @Override
+        public Builder reviewer(String reviewer) {
+            this.review.setReviewer(reviewer);
+            return this;
+        }
+
+        @Override
+        public Builder reviewerId(String reviewerId) {
+            this.review.setReviewerId(reviewerId);
+            return this;
+        }
+
+        @Override
+        public Builder tags(List<String> tags) {
+            this.review.setTags(tags);
+            return this;
+        }
+
+        @Override
+        public Builder body(String body) {
+            this.review.setBody(body);
+            return this;
+        }
+
+        @Override
+        public Builder rawBody(String rawBody) {
+            this.review.setRawBody(rawBody);
+            return this;
+        }
+
+        @Override
+        public Builder bodyType(BodyType bodyType) {
+            this.review.setBodyType(bodyType);
+            return this;
+        }
+
+        @Override
+        public Builder htmlBody() {
+            return this.bodyType(BodyType.HTML);
+        }
+
+        @Override
+        public Builder textBody() {
+            return this.bodyType(BodyType.TEXT);
+        }
+
+        @Override
+        public Builder jsonBody() {
+            return this.bodyType(BodyType.JSON);
+        }
+
+        @Override
+        public OrderReview build() {
+            return this.review;
+        }
     }
 }
