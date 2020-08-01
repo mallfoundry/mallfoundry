40168b8e09734fd79337c3e46bd2bbbaa4f27a0f
TangZhi <tgioer@163.com>
2020-08-01 16:48:14 +0800

    添加`ProductReviewIdentifier`。

---
+++ components/store/src/main/java/org/mallfoundry/catalog/product/review/ProductReviewIdentifier.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.catalog.product.review;
+
+import org.apache.commons.lang3.StringUtils;
+import org.mallfoundry.keygen.PrimaryKeyHolder;
+
+import java.util.List;
+
+public class ProductReviewIdentifier implements ProductReviewProcessor {
+    private static final String PRODUCT_REVIEW_VALUE_ID_NAME = "product.review.id";
+
+    @Override
+    public ProductReview preProcessApproveProductReview(ProductReview review) {
+        this.setIdentifier(review);
+        return review;
+    }
+
+    @Override
+    public List<ProductReview> preProcessAddProductReviews(List<ProductReview> reviews) {
+        reviews.forEach(this::setIdentifier);
+        return reviews;
+    }
+
+    private void setIdentifier(ProductReview review) {
+        if (StringUtils.isBlank(review.getId())) {
+            review.setId(PrimaryKeyHolder.next(PRODUCT_REVIEW_VALUE_ID_NAME));
+        }
+    }
+}
