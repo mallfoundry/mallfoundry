3709262cd38ac0337823693da6aa8361f5dd6006
TangZhi <tgioer@163.com>
2020-08-02 21:01:15 +0800

    重构`following`模块。

--- components/store-rest/src/main/java/org/mallfoundry/rest/follow/FollowResourceV1.java
+++ components/store-rest/src/main/java/org/mallfoundry/rest/follow/FollowResourceV1.java
@@ -19,9 +19,9 @@
 package org.mallfoundry.rest.follow;
 
 import org.mallfoundry.data.SliceList;
-import org.mallfoundry.follow.FollowProduct;
-import org.mallfoundry.follow.FollowService;
-import org.mallfoundry.follow.FollowStore;
+ import org.mallfoundry.following.FollowProduct;
+import org.mallfoundry.following.FollowService;
+import org.mallfoundry.following.FollowStore;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,13 +41,13 @@ public class FollowResourceV1 {
 
     @PostMapping("/customers/{customer_id}/following-products/{product_id}")
     public FollowProduct followProduct(@PathVariable("customer_id") String customerId,
-                                          @PathVariable("product_id") String productId) {
+                                       @PathVariable("product_id") String productId) {
         return this.followService.followProduct(customerId, productId);
     }
 
     @DeleteMapping("/customers/{customer_id}/following-products/{product_id}")
     public void unfollowProduct(@PathVariable("customer_id") String customerId,
-                                   @PathVariable("product_id") String productId) {
+                                @PathVariable("product_id") String productId) {
         this.followService.unfollowProduct(customerId, productId);
     }
 
@@ -76,13 +76,13 @@ public class FollowResourceV1 {
 
     @PostMapping("/customers/{customer_id}/following-stores/{store_id}")
     public void followStore(@PathVariable("customer_id") String customerId,
-                               @PathVariable("store_id") String storeId) {
+                            @PathVariable("store_id") String storeId) {
         this.followService.followStore(customerId, storeId);
     }
 
     @DeleteMapping("/customers/{customer_id}/following-stores/{store_id}")
     public void unfollowStore(@PathVariable("customer_id") String customerId,
-                                 @PathVariable("store_id") String storeId) {
+                              @PathVariable("store_id") String storeId) {
         this.followService.unfollowStore(customerId, storeId);
     }
 
@@ -100,12 +100,13 @@ public class FollowResourceV1 {
 
     @GetMapping("/customers/{customer_id}/following-stores/count")
     public long getFollowingStoreCount(@PathVariable("customer_id") String customerId) {
-        return this.followService.getFollowingStoreCount(this.followService.createFollowStoreQuery()
-                .toBuilder().followerId(customerId).build());
+        return this.followService.countFollowingStores(
+                this.followService.createFollowStoreQuery()
+                        .toBuilder().followerId(customerId).build());
     }
-
-    @GetMapping("/stores/{store_id}/followers/count")
-    public long getStoreFollowerCount(@PathVariable("store_id") String storeId) {
-        return this.followService.getStoreFollowerCount(storeId);
-    }
+//
+//    @GetMapping("/stores/{store_id}/followers/count")
+//    public long getStoreFollowerCount(@PathVariable("store_id") String storeId) {
+//        return this.followService.getStoreFollowerCount(storeId);
+//    }
 }
