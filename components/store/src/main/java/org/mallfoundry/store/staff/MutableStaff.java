691a20d9a5f3fca6249b6e76e709260a65e95e7e
TangZhi <tgioer@163.com>
2020-08-11 13:20:35 +0800

    添加`setStoreId`方法。

--- components/store/src/main/java/org/mallfoundry/store/staff/MutableStaff.java
+++ components/store/src/main/java/org/mallfoundry/store/staff/MutableStaff.java
@@ -22,6 +22,8 @@ import java.util.Date;
 
 public interface MutableStaff extends Staff {
 
+    void setStoreId(String storeId);
+
     void setStatus(StaffStatus status);
 
     void setCreatedTime(Date createdTime);
