6211f052dacbe5bc5d4f3825353ae678f7413479
TangZhi <tgioer@163.com>
2020-08-11 13:31:05 +0800

    添加`StoreRolesInitializer`。

---
+++ components/store/src/main/java/org/mallfoundry/store/initializing/StoreRolesInitializer.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.store.initializing;
+
+import org.mallfoundry.store.Store;
+import org.mallfoundry.store.StoreId;
+import org.mallfoundry.store.security.RoleService;
+
+/**
+ * 商铺角色初始化。
+ *
+ * @author Zhi Tang
+ */
+public class StoreRolesInitializer implements StoreInitializer {
+
+    private final RoleService roleService;
+
+    public StoreRolesInitializer(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @Override
+    public int getPosition() {
+        return POSITION_STEP * 2;
+    }
+
+    @Override
+    public void doInitialize(Store store) {
+        var storeId = store.toId();
+        this.clearRoles(storeId);
+        this.addSuperRole(storeId);
+        this.addGeneralRoles(storeId);
+    }
+
+    private void clearRoles(StoreId storeId) {
+        this.roleService.clearRoles(storeId);
+    }
+
+    private void addSuperRole(StoreId storeId) {
+        var superRole = this.roleService.createSuperRole(storeId);
+        superRole = this.roleService.addRole(superRole);
+        this.roleService.changeSuperRole(superRole.toId());
+    }
+
+    private void addGeneralRoles(StoreId storeId) {
+
+    }
+}
