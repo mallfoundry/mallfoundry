2a820493de856ef03ac769f6df311040da9af2fc
TangZhi <tgioer@163.com>
2020-08-15 10:04:09 +0800

    添加国际化信息。

---
+++ components/keystone/src/main/java/org/mallfoundry/security/access/AccessMessages.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.security.access;
+
+import static org.mallfoundry.i18n.MessageHolder.message;
+
+public abstract class AccessMessages {
+    private static final String SECURITY_ACCESS_CONTROL_NOT_FOUND_MESSAGE_CODE_KEY = "security.access.AccessControl.notFound";
+    private static final String SECURITY_ACCESS_PRINCIPAL_NOT_FOUND_MESSAGE_CODE_KEY = "security.access.Principal.notFound";
+    private static final String SECURITY_ACCESS_RESOURCE_NOT_FOUND_MESSAGE_CODE_KEY = "security.access.Resource.notFound";
+
+    public abstract static class AccessControl {
+        public static String notFound(String type, String name) {
+            return message(SECURITY_ACCESS_CONTROL_NOT_FOUND_MESSAGE_CODE_KEY, String.format("AccessControl %s %s not found", type, name));
+        }
+    }
+
+    public abstract static class Principal {
+        public static String notFound(String type, String name) {
+            return message(SECURITY_ACCESS_PRINCIPAL_NOT_FOUND_MESSAGE_CODE_KEY, String.format("Principal %s %s not found", type, name));
+        }
+    }
+
+    public abstract static class Resource {
+        public static String notFound(String type, String name) {
+            return message(SECURITY_ACCESS_RESOURCE_NOT_FOUND_MESSAGE_CODE_KEY, String.format("Resource %s %s not found", type, name));
+        }
+    }
+}
