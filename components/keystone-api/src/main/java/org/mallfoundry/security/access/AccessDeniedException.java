34d1e2f69c47dce16a6d761d825692d3eaa5162f
TangZhi <tgioer@163.com>
2020-08-15 10:04:30 +0800

    添加空行。

--- components/keystone-api/src/main/java/org/mallfoundry/security/access/AccessDeniedException.java
+++ components/keystone-api/src/main/java/org/mallfoundry/security/access/AccessDeniedException.java
@@ -21,6 +21,7 @@ package org.mallfoundry.security.access;
 import org.mallfoundry.security.SecurityException;
 
 public class AccessDeniedException extends SecurityException {
+
     public AccessDeniedException(String message) {
         super(message);
     }
