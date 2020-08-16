a2ddbb3d25dc639e978063ae434e8bd8f02e7583
TangZhi <tgioer@163.com>
2020-08-16 19:14:31 +0800

    移动包到`catalog.collection`。

--- components/store-rest/src/main/java/org/mallfoundry/rest/catalog/collection/CollectionRequest.java
+++ components/store-rest/src/main/java/org/mallfoundry/rest/catalog/collection/CollectionRequest.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-package org.mallfoundry.rest.store;
+package org.mallfoundry.rest.catalog.collection;
 
 import lombok.Getter;
 import lombok.Setter;
--- components/store-rest/src/main/java/org/mallfoundry/rest/catalog/collection/CollectionResourceV1.java
+++ components/store-rest/src/main/java/org/mallfoundry/rest/catalog/collection/CollectionResourceV1.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-package org.mallfoundry.rest.store;
+package org.mallfoundry.rest.catalog.collection;
 
 import org.mallfoundry.catalog.collection.ProductCollection;
 import org.mallfoundry.catalog.collection.ProductCollectionService;
