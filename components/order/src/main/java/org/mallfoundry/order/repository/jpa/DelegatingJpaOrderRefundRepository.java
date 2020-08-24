13e19909576aa462c0692b0d40ac692627a567f7
TangZhi <tgioer@163.com>
2020-08-24 18:29:28 +0800

    添加`OrderRefundRepository`。

---
+++ components/order/src/main/java/org/mallfoundry/order/repository/jpa/DelegatingJpaOrderRefundRepository.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.order.repository.jpa;
+
+import org.mallfoundry.data.PageList;
+import org.mallfoundry.data.SliceList;
+import org.mallfoundry.order.OrderRefund;
+import org.mallfoundry.order.OrderRefundQuery;
+import org.mallfoundry.order.OrderRefundRepository;
+
+public class DelegatingJpaOrderRefundRepository implements OrderRefundRepository {
+
+    private final JpaOrderRefundRepository repository;
+
+    public DelegatingJpaOrderRefundRepository(JpaOrderRefundRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public SliceList<OrderRefund> findAll(OrderRefundQuery query) {
+        var page = this.repository.findAll(query);
+        return PageList.of(page.getContent())
+                .page(query.getPage()).limit(query.getLimit())
+                .totalSize(page.getTotalElements())
+                .cast();
+    }
+
+    @Override
+    public long count(OrderRefundQuery query) {
+        return this.repository.count(query);
+    }
+}
---
+++ components/order/src/main/java/org/mallfoundry/order/repository/jpa/JpaOrderRefundRepository.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.order.repository.jpa;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.mallfoundry.order.OrderRefundQuery;
+import org.mallfoundry.util.CaseUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import javax.persistence.criteria.Predicate;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public interface JpaOrderRefundRepository extends JpaRepository<JpaOrderRefund, String>, JpaSpecificationExecutor<JpaOrderRefund> {
+
+    default Specification<JpaOrderRefund> createSpecification(OrderRefundQuery refundQuery) {
+        return (Specification<JpaOrderRefund>) (root, query, criteriaBuilder) -> {
+            Predicate predicate = criteriaBuilder.conjunction();
+            if (Objects.nonNull(refundQuery.getCustomerId())) {
+                predicate.getExpressions().add(criteriaBuilder.equal(root.get("customerId"), refundQuery.getCustomerId()));
+            }
+            if (Objects.nonNull(refundQuery.getStoreId())) {
+                predicate.getExpressions().add(criteriaBuilder.equal(root.get("storeId"), refundQuery.getStoreId()));
+            }
+            if (CollectionUtils.isNotEmpty(refundQuery.getStatuses())) {
+                predicate.getExpressions().add(criteriaBuilder.in(root.get("status")).value(refundQuery.getStatuses()));
+            }
+            return predicate;
+        };
+    }
+
+    private Sort createSort(OrderRefundQuery query) {
+        return Optional.ofNullable(query.getSort())
+                .map(org.mallfoundry.data.Sort::getOrders)
+                .filter(CollectionUtils::isNotEmpty)
+                .map(orders -> Sort.by(orders.stream()
+                        .peek(sortOrder -> sortOrder.setProperty(CaseUtils.camelCase(sortOrder.getProperty())))
+                        .map(sortOrder -> sortOrder.getDirection().isDescending()
+                                ? Sort.Order.desc(sortOrder.getProperty())
+                                : Sort.Order.asc(sortOrder.getProperty()))
+                        .collect(Collectors.toUnmodifiableList())))
+                .orElseGet(() -> Sort.by("appliedTime").descending());
+    }
+
+    default Page<JpaOrderRefund> findAll(OrderRefundQuery query) {
+        var sort = this.createSort(query);
+        return this.findAll(this.createSpecification(query), PageRequest.of(query.getPage() - 1, query.getLimit(), sort));
+    }
+
+    default long count(OrderRefundQuery query) {
+        return this.count(this.createSpecification(query));
+    }
+}
