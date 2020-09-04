c0faf4d22c797278ebe47ece4b8e0fbf77707bf3
TangZhi <tgioer@163.com>
2020-09-04 10:24:59 +0800

    添加`FileBlob`。

--- components/storage/src/main/java/org/mallfoundry/storage/FileBlob.java
+++ components/storage/src/main/java/org/mallfoundry/storage/FileBlob.java
@@ -16,7 +16,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-package org.mallfoundry.util;
+package org.mallfoundry.storage;
 
-public interface Configuration {
+public abstract class FileBlob extends BlobSupport {
+
 }
