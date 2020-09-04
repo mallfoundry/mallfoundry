e1ce15fca03dba4c7a94450e2dcf9e3ef9418202
TangZhi <tgioer@163.com>
2020-09-04 10:30:06 +0800

    添加`BlobIdRequest`。

--- components/storage-rest/src/main/java/org/mallfoundry/rest/storage/BlobIdRequest.java
+++ components/storage-rest/src/main/java/org/mallfoundry/rest/storage/BlobIdRequest.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-package org.mallfoundry.rest.store.blob;
+package org.mallfoundry.rest.storage;
 
 import lombok.Getter;
 import lombok.Setter;
