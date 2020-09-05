97a898b1c62fc824c005c573a92c324e3ba12188
TangZhi <tgioer@163.com>
2020-09-05 14:52:48 +0800

    实现`StorePostService`。

---
+++ components/store/src/main/java/org/mallfoundry/store/StorePostService.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.store;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.transaction.annotation.Transactional;
+
+public class StorePostService implements ApplicationEventPublisherAware {
+
+    private ApplicationEventPublisher eventPublisher;
+
+    private final StoreRepository storeRepository;
+
+    public StorePostService(StoreRepository storeRepository) {
+        this.storeRepository = storeRepository;
+    }
+
+    @Transactional
+    public void initializeStore(Store store) {
+        store = this.storeRepository.save(store);
+        this.eventPublisher.publishEvent(new ImmutableStoreInitializedEvent(store));
+    }
+
+    @Transactional
+    public void closeStore(Store store) {
+        this.storeRepository.delete(store);
+        this.eventPublisher.publishEvent(new ImmutableStoreClosedEvent(store));
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
+    }
+}
