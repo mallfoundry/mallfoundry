7461a88f62262d092680de2f3df01067b308cf9a
TangZhi <tgioer@163.com>
2020-09-10 15:44:31 +0800

    完善`OrderReviewQuery`。

--- components/order/src/main/java/org/mallfoundry/order/review/repository/jpa/JpaOrderReviewRepository.java
+++ components/order/src/main/java/org/mallfoundry/order/review/repository/jpa/JpaOrderReviewRepository.java
@@ -18,28 +18,32 @@
 
 package org.mallfoundry.order.review.repository.jpa;
 
-import org.mallfoundry.order.aftersales.repository.jpa.JpaOrderDispute;
+import org.apache.commons.collections4.CollectionUtils;
 import org.mallfoundry.order.repository.jpa.JpaOrderReview;
 import org.mallfoundry.order.review.OrderReviewQuery;
+import org.mallfoundry.util.CaseUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import javax.persistence.criteria.Predicate;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public interface JpaOrderReviewRepository extends JpaRepository<JpaOrderReview, String>, JpaSpecificationExecutor<JpaOrderReview> {
 
     default Specification<JpaOrderReview> createSpecification(OrderReviewQuery reviewQuery) {
         return (Specification<JpaOrderReview>) (root, query, criteriaBuilder) -> {
             Predicate predicate = criteriaBuilder.conjunction();
-            if (Objects.nonNull(reviewQuery.getStoreId())) {
-                predicate.getExpressions().add(criteriaBuilder.equal(root.get("storeId"), reviewQuery.getStoreId()));
-            }
             if (Objects.nonNull(reviewQuery.getCustomerId())) {
                 predicate.getExpressions().add(criteriaBuilder.equal(root.get("customerId"), reviewQuery.getCustomerId()));
+            }
+            if (Objects.nonNull(reviewQuery.getStoreId())) {
+                predicate.getExpressions().add(criteriaBuilder.equal(root.get("storeId"), reviewQuery.getStoreId()));
             }
             if (Objects.nonNull(reviewQuery.getOrderId())) {
                 predicate.getExpressions().add(criteriaBuilder.equal(root.get("orderId"), reviewQuery.getOrderId()));
@@ -48,8 +52,22 @@ public interface JpaOrderReviewRepository extends JpaRepository<JpaOrderReview, 
         };
     }
 
+    private Sort createSort(OrderReviewQuery query) {
+        return Optional.ofNullable(query.getSort())
+                .map(org.mallfoundry.data.Sort::getOrders)
+                .filter(CollectionUtils::isNotEmpty)
+                .map(orders -> Sort.by(orders.stream()
+                        .peek(sortOrder -> sortOrder.setProperty(CaseUtils.camelCase(sortOrder.getProperty())))
+                        .map(sortOrder -> sortOrder.getDirection().isDescending()
+                                ? Sort.Order.desc(sortOrder.getProperty())
+                                : Sort.Order.asc(sortOrder.getProperty()))
+                        .collect(Collectors.toUnmodifiableList())))
+                .orElseGet(() -> Sort.by("reviewedTime").descending());
+    }
+
     default Page<JpaOrderReview> findAll(OrderReviewQuery query) {
-        return this.findAll(this.createSpecification(query), PageRequest.of(query.getPage() - 1, query.getLimit()));
+        return this.findAll(this.createSpecification(query),
+                PageRequest.of(query.getPage() - 1, query.getLimit(), this.createSort(query)));
     }
 
 }
