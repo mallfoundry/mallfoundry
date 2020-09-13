e50841976ad64c7bbfd55ae34e33e3ddcd5ea7d4
TangZhi <tgioer@163.com>
2020-09-13 16:00:53 +0800

    添加`CouponMessages`。

---
+++ components/marketing/src/main/java/org/mallfoundry/marketing/coupon/CouponMessages.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.marketing.coupon;
+
+import org.mallfoundry.i18n.Messages;
+
+import static org.mallfoundry.i18n.MessageHolder.message;
+
+public abstract class CouponMessages {
+
+    private static final String COUPON_NOT_FOUND_MESSAGE_CODE_KEY = Messages.getKeys(Coupon.class).codeKey("notFound");
+
+    public static String notFound() {
+        return message(COUPON_NOT_FOUND_MESSAGE_CODE_KEY, "Coupon does not exist");
+    }
+
+}
