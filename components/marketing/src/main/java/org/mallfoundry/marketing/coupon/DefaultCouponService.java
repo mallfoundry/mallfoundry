cf4b4267e68e6179a0029cb5027a4f1d49e71f54
TangZhi <tgioer@163.com>
2020-09-13 16:02:10 +0800

    完善`DefaultCouponService`。

--- components/marketing/src/main/java/org/mallfoundry/marketing/coupon/DefaultCouponService.java
+++ components/marketing/src/main/java/org/mallfoundry/marketing/coupon/DefaultCouponService.java
@@ -18,30 +18,47 @@
 
 package org.mallfoundry.marketing.coupon;
 
+import org.apache.commons.lang3.StringUtils;
 import org.mallfoundry.data.SliceList;
+import org.mallfoundry.processor.Processors;
+import org.mallfoundry.security.SubjectHolder;
 import org.springframework.transaction.annotation.Transactional;
 
-public class DefaultCouponService implements CouponService {
+import java.util.List;
+
+public class DefaultCouponService implements CouponService, CouponProcessorInvoker {
 
     private final CouponRepository couponRepository;
 
-    public DefaultCouponService(CouponRepository couponRepository) {
+    private final TakeCouponRepository takeCouponRepository;
+
+    private List<CouponProcessor> processors;
+
+    public DefaultCouponService(CouponRepository couponRepository,
+                                TakeCouponRepository takeCouponRepository) {
         this.couponRepository = couponRepository;
+        this.takeCouponRepository = takeCouponRepository;
+    }
+
+    public void setProcessors(List<CouponProcessor> processors) {
+        this.processors = processors;
     }
 
     @Override
-    public CouponId createCouponId(String couponId) {
-        return new ImmutableCouponId(couponId);
+    public CouponQuery createCouponQuery() {
+        return new DefaultCouponQuery();
     }
 
     @Override
-    public Coupon createCoupon(CouponId couponId) {
+    public Coupon createCoupon(String couponId) {
         return this.couponRepository.create(couponId);
     }
 
     @Transactional
     @Override
     public Coupon addCoupon(Coupon coupon) {
+        coupon = this.invokePreProcessBeforeAddCoupon(coupon);
+        coupon.create();
         return this.couponRepository.save(coupon);
     }
 
@@ -50,36 +67,80 @@ public class DefaultCouponService implements CouponService {
         return this.couponRepository.findAll(query);
     }
 
-    private Coupon requiredCoupon(CouponId couponId) {
-        return this.couponRepository.findById(couponId).orElseThrow();
+    private Coupon requiredCoupon(String couponId) {
+        return this.couponRepository.findById(couponId)
+                .orElseThrow(() -> new CouponException(CouponMessages.notFound()));
     }
 
     @Transactional
     @Override
     public void updateCoupon(Coupon source) {
-        var coupon = this.requiredCoupon(source.toId());
+        var coupon = this.requiredCoupon(source.getId());
         this.couponRepository.save(coupon);
     }
 
     @Transactional
     @Override
-    public void deleteCoupon(CouponId couponId) {
+    public void deleteCoupon(String couponId) {
         var coupon = this.requiredCoupon(couponId);
         this.couponRepository.delete(coupon);
     }
 
     @Override
-    public ReceiveCoupon createReceiveCoupon() {
-        return null;
+    public TakeCoupon createTakeCoupon(String takeId) {
+        return this.takeCouponRepository.create(takeId);
+    }
+
+    private void tryTakeLimit(Coupon coupon, TakeCoupon takeCoupon) {
+        // 每个顾客领取的上限。
+        if (0 < coupon.getTakeLimitPerCustomer()) {
+            var takeCount = this.takeCouponRepository.count(
+                    this.createCouponQuery().toBuilder()
+                            .customerId(takeCoupon.getCustomerId())
+                            .storeId(coupon.getStoreId()).build());
+            if (coupon.getTakeLimitPerCustomer() <= takeCount) {
+                throw new TakeCouponException("");
+            }
+        }
+    }
+
+    @Transactional
+    @Override
+    public TakeCoupon takeCoupon(TakeCoupon takeCoupon) throws TakeCouponException {
+        var coupon = this.requiredCoupon(takeCoupon.getCouponId());
+        // 如果没有设置顾客标识，使用当前用户标识。
+        if (StringUtils.isBlank(takeCoupon.getCustomerId())) {
+            takeCoupon.setCustomerId(SubjectHolder.getSubject().getId());
+        }
+        takeCoupon = this.invokePreProcessBeforeTakeCoupon(coupon, takeCoupon);
+        // 判断是否符合领取条件。
+        this.tryTakeLimit(coupon, takeCoupon);
+        takeCoupon = coupon.take(takeCoupon);
+        this.couponRepository.save(coupon);
+        return this.takeCouponRepository.save(takeCoupon);
     }
 
     @Override
-    public ReceiveCoupon receiveCoupon(ReceiveCoupon receiveCoupon) {
-        return null;
+    public SliceList<TakeCoupon> getTakeCoupons(CouponQuery query) {
+        return this.takeCouponRepository.findAll(query);
     }
 
     @Override
-    public SliceList<ReceiveCoupon> getReceiveCoupons(CouponQuery query) {
-        return null;
+    public long countTakeCoupons(CouponQuery query) {
+        return this.takeCouponRepository.count(query);
+    }
+
+    @Override
+    public Coupon invokePreProcessBeforeAddCoupon(Coupon coupon) {
+        return Processors.stream(this.processors)
+                .map(CouponProcessor::preProcessBeforeAddCoupon)
+                .apply(coupon);
+    }
+
+    @Override
+    public TakeCoupon invokePreProcessBeforeTakeCoupon(Coupon coupon, TakeCoupon receiveCoupon) {
+        return Processors.stream(this.processors)
+                .<TakeCoupon>map((processor, identity) -> processor.preProcessBeforeTakeCoupon(coupon, identity))
+                .apply(receiveCoupon);
     }
 }
