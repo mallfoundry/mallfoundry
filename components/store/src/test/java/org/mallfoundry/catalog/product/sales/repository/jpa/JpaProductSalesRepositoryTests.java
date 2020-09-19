3ee945143ee0f19260035483d5cf2b912cc6e958
TangZhi <tgioer@163.com>
2020-09-19 18:12:47 +0800

    添加`JpaProductSalesRepositoryTests`。

---
+++ components/store/src/test/java/org/mallfoundry/catalog/product/sales/repository/jpa/JpaProductSalesRepositoryTests.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.catalog.product.sales.repository.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.mallfoundry.catalog.product.sales.DefaultProductSalesQuery;
+import org.mallfoundry.test.StandaloneTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@StandaloneTest
+public class JpaProductSalesRepositoryTests {
+
+    @Autowired
+    private DelegatingJpaProductSalesRepository repository;
+
+    @Transactional
+    @Test
+    public void testSumQuantities() {
+        var endDate = LocalDate.now();
+        var startDate = endDate.minusDays(30);
+        // start date
+        var yearStart = (short) startDate.getYear();
+        var monthStart = (byte) startDate.getMonthValue();
+        var dayOfMonthStart = (byte) startDate.getDayOfMonth();
+        // end date
+        var yearEnd = (short) endDate.getYear();
+        var monthEnd = (byte) endDate.getMonthValue();
+        var dayOfMonthEnd = (byte) endDate.getDayOfMonth();
+        var query = new DefaultProductSalesQuery()
+                .toBuilder()
+                .yearStart(yearStart).monthStart(monthStart).dayOfMonthStart(dayOfMonthStart)
+                .yearEnd(yearEnd).monthEnd(monthEnd).dayOfMonthEnd(dayOfMonthEnd)
+                .build();
+        query.setProductId("0");
+        var quantities = this.repository.sumQuantities(query);
+        System.out.println(quantities);
+    }
+}
