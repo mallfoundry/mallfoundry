16e6001debdd05b7cdb9588a2ac8a2b92a75a88d
TangZhi <tgioer@163.com>
2020-09-22 10:56:22 +0800

    完善`viewPage`。

--- components/content/src/main/java/org/mallfoundry/page/DefaultPageService.java
+++ components/content/src/main/java/org/mallfoundry/page/DefaultPageService.java
@@ -23,7 +23,13 @@ import org.mallfoundry.processor.Processors;
 import org.mallfoundry.security.SubjectHolder;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.chrono.ChronoZonedDateTime;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 public class DefaultPageService implements PageService, PageProcessorInvoker {
 
@@ -37,8 +43,35 @@ public class DefaultPageService implements PageService, PageProcessorInvoker {
     }
 
     @Override
+    public PageViewQuery createPageViewQuery() {
+        return new DefaultPageViewQuery();
+    }
+
+    @Override
     public PageView createPageView(String id) {
         return this.pageViewRepository.create(id);
+    }
+
+    private PageView getLatestViewPage(PageView pageView) {
+        var latestBrowsingTime = Optional.of(pageView)
+                .map(PageView::getBrowsingTime)
+                .map(Date::toInstant)
+                .map(instant -> instant.atZone(ZoneId.systemDefault()))
+                .map(ZonedDateTime::toLocalDateTime)
+                .map(time -> time.minusSeconds(10)) // 10s 窗口。
+                .map(time -> time.atZone(ZoneId.systemDefault()))
+                .map(ChronoZonedDateTime::toInstant)
+                .map(Date::from)
+                .orElseThrow();
+        var query = this.createPageViewQuery().toBuilder()
+                .pageId(pageView.getPageId())
+                .browserId(pageView.getBrowserId())
+                .browserIp(pageView.getBrowserIp())
+                .browsingTimeFrom(latestBrowsingTime)
+                .sort(sort -> sort.desc("browsingTime"))
+                .build();
+        return this.pageViewRepository.findAll(query)
+                .getElements().stream().findFirst().orElse(null);
     }
 
     @Transactional
@@ -47,6 +80,11 @@ public class DefaultPageService implements PageService, PageProcessorInvoker {
         pageView = this.invokePreProcessBeforeViewPage(pageView);
         pageView.setBrowserId(SubjectHolder.getSubject().getId());
         pageView.browsing();
+        var latestViewPage = this.getLatestViewPage(pageView);
+        if (Objects.nonNull(latestViewPage)) {
+            return latestViewPage;
+        }
+        pageView = this.invokePreProcessAfterViewPage(pageView);
         return this.pageViewRepository.save(pageView);
     }
 
@@ -54,6 +92,13 @@ public class DefaultPageService implements PageService, PageProcessorInvoker {
     public PageView invokePreProcessBeforeViewPage(PageView pageView) {
         return Processors.stream(this.processors)
                 .map(PageProcessor::preProcessBeforeViewPage)
+                .apply(pageView);
+    }
+
+    @Override
+    public PageView invokePreProcessAfterViewPage(PageView pageView) {
+        return Processors.stream(this.processors)
+                .map(PageProcessor::preProcessAfterViewPage)
                 .apply(pageView);
     }
 }
