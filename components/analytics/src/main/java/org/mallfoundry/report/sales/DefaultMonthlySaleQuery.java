39579a9d816970dd510ac3e8b5b2bb78b382e00a
TangZhi <tgioer@163.com>
2020-09-24 12:33:21 +0800

    添加`DefaultMonthlySaleQuery`。

---
+++ components/analytics/src/main/java/org/mallfoundry/report/sales/DefaultMonthlySaleQuery.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.report.sales;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DefaultMonthlySaleQuery implements MonthlySaleQuery {
+
+    private String tenantId;
+
+    private String storeId;
+
+    private String customerId;
+
+    private Short year;
+
+    private Byte month;
+
+    @Override
+    public Builder toBuilder() {
+        return new BuilderSupport(this) {
+        };
+    }
+
+    protected abstract static class BuilderSupport implements Builder {
+
+        private final DefaultMonthlySaleQuery query;
+
+        protected BuilderSupport(DefaultMonthlySaleQuery query) {
+            this.query = query;
+        }
+
+        @Override
+        public Builder tenantId(String tenantId) {
+            this.query.setTenantId(tenantId);
+            return this;
+        }
+
+        @Override
+        public Builder storeId(String storeId) {
+            this.query.setStoreId(storeId);
+            return this;
+        }
+
+        @Override
+        public Builder customerId(String customerId) {
+            this.query.setCustomerId(customerId);
+            return this;
+        }
+
+        @Override
+        public Builder year(Short year) {
+            this.query.setYear(year);
+            return this;
+        }
+
+        @Override
+        public Builder month(Byte month) {
+            this.query.setMonth(month);
+            return this;
+        }
+
+        @Override
+        public MonthlySaleQuery build() {
+            return this.query;
+        }
+    }
+}
