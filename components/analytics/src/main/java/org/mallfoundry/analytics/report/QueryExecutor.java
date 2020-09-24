005144ca75ea57a119cee46e1b7011e2cfd36e97
TangZhi <tgioer@163.com>
2020-09-24 12:35:57 +0800

    删除多余导入。

--- components/analytics/src/main/java/org/mallfoundry/analytics/report/QueryExecutor.java
+++ components/analytics/src/main/java/org/mallfoundry/analytics/report/QueryExecutor.java
@@ -18,11 +18,6 @@
 
 package org.mallfoundry.analytics.report;
 
-import org.mallfoundry.analytics.report.QueryResult;
-import org.mallfoundry.analytics.report.Report;
-import org.mallfoundry.analytics.report.ReportQuery;
-
 public interface QueryExecutor {
-
     QueryResult execute(ReportQuery query, Report report);
 }
