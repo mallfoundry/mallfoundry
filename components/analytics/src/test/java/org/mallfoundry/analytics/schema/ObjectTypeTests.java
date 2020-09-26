eec360a24654819066177b4e99642fa5466a7dd9
TangZhi <tgioer@163.com>
2020-09-26 10:21:02 +0800

    优化导入。

--- components/analytics/src/test/java/org/mallfoundry/analytics/schema/ObjectTypeTests.java
+++ components/analytics/src/test/java/org/mallfoundry/analytics/schema/ObjectTypeTests.java
@@ -19,7 +19,6 @@
 package org.mallfoundry.analytics.schema;
 
 import org.junit.jupiter.api.Test;
-import org.mallfoundry.analytics.schema.ObjectTypeManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
