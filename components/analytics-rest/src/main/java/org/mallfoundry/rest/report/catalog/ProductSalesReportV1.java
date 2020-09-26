cf508ab8d935e44437fc9b668bf3442133ef414f
TangZhi <tgioer@163.com>
2020-09-26 10:20:23 +0800

    添加`ProductSalesReportV1`。

---
+++ components/analytics-rest/src/main/java/org/mallfoundry/rest/report/catalog/ProductSalesReportV1.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.rest.report.catalog;
+
+import org.mallfoundry.data.SliceList;
+import org.mallfoundry.report.catalog.ProductSale;
+import org.mallfoundry.report.catalog.ProductSalesReport;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/reports")
+public class ProductSalesReportV1 {
+
+    private final ProductSalesReport productSalesReport;
+
+    public ProductSalesReportV1(ProductSalesReport productSalesReport) {
+        this.productSalesReport = productSalesReport;
+    }
+
+    @GetMapping("/product-sales")
+    public SliceList<ProductSale> queryProductSales(@RequestParam(required = false) Short year,
+                                                    @RequestParam(required = false) Byte month,
+                                                    @RequestParam(required = false, name = "date_from") Integer dateFrom,
+                                                    @RequestParam(required = false, name = "date_to") Integer dateTo,
+                                                    @RequestParam(required = false, name = "tenant_id") String tenantId,
+                                                    @RequestParam(required = false, name = "store_id") String storeId,
+                                                    @RequestParam(required = false, name = "customer_id") String customerId) {
+        return this.productSalesReport.queryProductSales(
+                this.productSalesReport.createProductSalesQuery().toBuilder()
+                        .year(year).month(month)
+                        .dateFrom(dateFrom).dateTo(dateTo)
+                        .tenantId(tenantId).storeId(storeId).customerId(customerId)
+                        .build());
+    }
+}
