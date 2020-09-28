c26ea650fbe0856eb833c9da09990a0291c0d922
TangZhi <tgioer@163.com>
2020-09-28 14:50:57 +0800

    添加`DefaultProductCountQuery`。

---
+++ components/analytics/src/main/java/org/mallfoundry/report/catalog/DefaultProductCountQuery.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.report.catalog;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DefaultProductCountQuery implements ProductCountQuery {
+
+    private String tenantId;
+
+    private String storeId;
+
+    private String customerId;
+
+    private Short year;
+
+    private Byte month;
+
+    private Integer date;
+
+    private Integer dateFrom;
+
+    private Integer dateTo;
+
+    @Override
+    public Builder toBuilder() {
+        return new BuilderSupport(this) {
+        };
+    }
+
+    protected abstract static class BuilderSupport implements Builder {
+
+        private final DefaultProductCountQuery query;
+
+        protected BuilderSupport(DefaultProductCountQuery query) {
+            this.query = query;
+        }
+
+        @Override
+        public Builder tenantId(String tenantId) {
+            this.query.setTenantId(tenantId);
+            return this;
+        }
+
+        @Override
+        public Builder storeId(String storeId) {
+            this.query.setStoreId(storeId);
+            return this;
+        }
+
+        @Override
+        public Builder customerId(String customerId) {
+            this.query.setCustomerId(customerId);
+            return this;
+        }
+
+        @Override
+        public Builder year(Short year) {
+            this.query.setYear(year);
+            return this;
+        }
+
+        @Override
+        public Builder month(Byte month) {
+            this.query.setMonth(month);
+            return this;
+        }
+
+        @Override
+        public Builder date(Integer date) {
+            this.query.setDate(date);
+            return this;
+        }
+
+        @Override
+        public Builder dateFrom(Integer dateFrom) {
+            this.query.setDateFrom(dateFrom);
+            return this;
+        }
+
+        @Override
+        public Builder dateTo(Integer dateTo) {
+            this.query.setDateTo(dateTo);
+            return this;
+        }
+
+        @Override
+        public ProductCountQuery build() {
+            return this.query;
+        }
+    }
+}
