53498093800ecae4cd237c53d037d1395df31864
TangZhi <tgioer@163.com>
2020-10-15 16:53:07 +0800

    添加`Balance`。

---
+++ components/trade-api/src/main/java/org/mallfoundry/trade/Balance.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.trade;
+
+import java.math.BigDecimal;
+
+public interface Balance {
+
+    // 不可用余额
+    BalanceAmount getPending();
+
+    //可用余额
+    BalanceAmount getAvailable();
+
+    // from 取
+    void debit(BigDecimal amount);
+
+    // to 存
+    void credit(BigDecimal amount);
+
+    void freeze(BigDecimal amount);
+
+    void unfreeze(BigDecimal amount);
+}
