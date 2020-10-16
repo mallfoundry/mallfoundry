5a1df493e8ed09043506c05584b45c1c807aeae0
TangZhi <tgioer@163.com>
2020-10-16 16:52:50 +0800

    添加 AccountResourceV1 接口。

---
+++ components/trade-rest/src/main/java/org/mallfoundry/rest/trade/AccountResourceV1.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.rest.trade;
+
+import org.mallfoundry.trade.Account;
+import org.mallfoundry.trade.AccountService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class AccountResourceV1 {
+
+    private final AccountService accountService;
+
+    public AccountResourceV1(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/accounts/{account_id}")
+    public Account getAccount(@PathVariable(name = "account_id") String accountId) {
+        return this.accountService.getAccount(accountId);
+    }
+}
