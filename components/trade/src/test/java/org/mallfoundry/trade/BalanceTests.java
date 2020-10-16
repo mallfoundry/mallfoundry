386ab61b57471a47524d724a6a444bf06390a38c
TangZhi <tgioer@163.com>
2020-10-16 16:49:15 +0800

    添加`BalanceTests`测试用例。

---
+++ components/trade/src/test/java/org/mallfoundry/trade/BalanceTests.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.trade;
+
+import org.junit.jupiter.api.Test;
+import org.mallfoundry.trade.repository.jpa.JpaBalance;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+public class BalanceTests {
+
+    @Test
+    public void testBalanceCredit() {
+        var balance = new JpaBalance();
+        balance.credit(SourceType.ALIPAY, BigDecimal.valueOf(100));
+    }
+
+    @Test
+    public void testBalanceDebit() {
+        var balance = new JpaBalance();
+        balance.debit(SourceType.ALIPAY, BigDecimal.valueOf(100));
+    }
+
+    @Test
+    public void testGetFirst0NumericCodeAsString() {
+        var arsNumericCode = Currency.getInstance("ars").getNumericCodeAsString();
+        System.out.println(arsNumericCode);
+    }
+}
