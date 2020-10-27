a76b8176cdd5858d535c00f5f4a441be1ceb38bd
TangZhi <tgioer@163.com>
2020-10-27 16:50:21 +0800

    添加 DelegatingJpaBalanceRepository。

---
+++ components/finance/src/main/java/org/mallfoundry/finance/account/repository/jpa/DelegatingJpaBalanceRepository.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.finance.account.repository.jpa;
+
+import org.mallfoundry.finance.account.Balance;
+import org.mallfoundry.finance.account.BalanceId;
+import org.mallfoundry.finance.account.BalanceRepository;
+import org.mallfoundry.finance.account.ImmutableBalanceId;
+import org.springframework.data.util.CastUtils;
+
+import java.util.Optional;
+
+public class DelegatingJpaBalanceRepository implements BalanceRepository {
+
+    private final JpaBalanceRepository repository;
+
+    public DelegatingJpaBalanceRepository(JpaBalanceRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Balance create(BalanceId balanceId) {
+        return new JpaBalance(balanceId);
+    }
+
+    @Override
+    public Optional<Balance> findById(BalanceId balanceId) {
+        return CastUtils.cast(this.repository.findById(ImmutableBalanceId.of(balanceId)));
+    }
+
+    @Override
+    public Balance save(Balance balance) {
+        return this.repository.save(CastUtils.cast(balance));
+    }
+}
