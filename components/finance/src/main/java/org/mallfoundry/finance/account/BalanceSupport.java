679f04f1fe93e5ec50ec8fa3040aa2ebaa235a97
TangZhi <tgioer@163.com>
2020-10-27 16:56:14 +0800

    实现 withdraw 方法。

--- components/finance/src/main/java/org/mallfoundry/finance/account/BalanceSupport.java
+++ components/finance/src/main/java/org/mallfoundry/finance/account/BalanceSupport.java
@@ -18,10 +18,23 @@
 
 package org.mallfoundry.finance.account;
 
+import org.mallfoundry.finance.TransactionDirection;
+import org.mallfoundry.finance.TransactionType;
+import org.mallfoundry.util.DecimalUtils;
+
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public abstract class BalanceSupport implements MutableBalance {
+
+    @Override
+    public BalanceId toId() {
+        return new ImmutableBalanceId(this.getAccountId(), this.getCurrency());
+    }
 
     @Override
     public BalanceSource getSource(BalanceSourceType sourceType) {
@@ -31,13 +44,14 @@ public abstract class BalanceSupport implements MutableBalance {
                 .orElse(null);
     }
 
-    private void creditSource(BalanceSourceType sourceType, BigDecimal amount) {
+    private BalanceSource creditSource(BalanceSourceType sourceType, BigDecimal amount) {
         var source = this.getSource(sourceType);
         if (Objects.isNull(source)) {
             source = this.createSource(sourceType);
             this.getSources().add(source);
         }
         source.credit(amount);
+        return source;
     }
 
     private void debitSource(BalanceSourceType sourceType, BigDecimal amount) {
@@ -67,15 +81,62 @@ public abstract class BalanceSupport implements MutableBalance {
     }
 
     @Override
-    public void credit(BalanceSourceType sourceType, BigDecimal amount) {
-        this.creditSource(sourceType, amount);
+    public BalanceSource credit(BalanceSourceType sourceType, BigDecimal amount) {
+        var source = this.creditSource(sourceType, amount);
         this.setAvailableAmount(this.getAvailableAmount().add(amount));
+        return source;
     }
 
     @Override
     public void debit(BalanceSourceType sourceType, BigDecimal amount) {
         this.debitSource(sourceType, amount);
         this.setAvailableAmount(this.getAvailableAmount().subtract(amount));
+    }
+
+    private List<BalanceSource> getWithdrawalSources() {
+        return this.getSources().stream()
+                .sorted(Comparator.comparing(BalanceSource::getAmount, Comparator.reverseOrder()))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    @Override
+    public BalanceTransaction withdraw(BigDecimal amount) {
+        var debitingAmount = amount;
+        var debitingSources = new ArrayList<BalanceSource>();
+        for (var source : this.getWithdrawalSources()) {
+            var debitedSource = this.createSource(source.getType());
+            // debitingAmount <= sourceAmount, 扣减一次。
+            // debitingAmount > sourceAmount, 扣减 sourceAmount。
+            if (DecimalUtils.lessThanOrEqualTo(debitingAmount, source.getAmount())) {
+                source.debit(debitingAmount);
+                debitedSource.credit(debitingAmount);
+                debitingAmount = BigDecimal.ZERO;
+            } else {
+                var debitedAmount = source.getAmount();
+                source.debit(debitedAmount);
+                debitedSource.credit(debitedAmount);
+                debitingAmount = debitingAmount.subtract(debitedAmount);
+            }
+            debitingSources.add(debitedSource);
+            if (DecimalUtils.equals(debitingAmount, BigDecimal.ZERO)) {
+                break;
+            }
+        }
+        this.setAvailableAmount(this.getAvailableAmount().subtract(amount));
+        return this.withdrawTransaction(amount, debitingSources);
+    }
+
+    private BalanceTransaction withdrawTransaction(BigDecimal amount, List<BalanceSource> debitingSources) {
+        var transaction = this.createTransaction();
+        transaction.setAccountId(this.getAccountId());
+        transaction.setCurrency(this.getCurrency());
+        transaction.setDirection(TransactionDirection.DEBIT);
+        transaction.setType(TransactionType.WITHDRAWAL);
+        transaction.setAmount(amount);
+        transaction.setSources(debitingSources);
+        transaction.setEndingBalance(this.getAvailableAmount());
+        transaction.create();
+        return transaction;
     }
 
     @Override
