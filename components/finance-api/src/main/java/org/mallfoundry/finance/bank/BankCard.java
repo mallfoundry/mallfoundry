1943bfefb2c8a14483036753befac3d866df48eb
TangZhi <tgioer@163.com>
2020-11-05 16:14:42 +0800

    添加 toBuilder 方法。

--- components/finance-api/src/main/java/org/mallfoundry/finance/bank/BankCard.java
+++ components/finance-api/src/main/java/org/mallfoundry/finance/bank/BankCard.java
@@ -18,32 +18,82 @@
 
 package org.mallfoundry.finance.bank;
 
+import org.mallfoundry.util.ObjectBuilder;
+
 import java.util.Date;
 
-public interface BankCard {
+public interface BankCard extends ObjectBuilder.ToBuilder<BankCard.Builder> {
 
     String getId();
 
+    void setId(String id);
+
     String getAccountId();
+
+    void setAccountId(String accountId);
 
     BankCardFunding getFunding();
 
+    void setFunding(BankCardFunding funding);
+
     String getBankName();
+
+    void setBankName(String bankName);
 
     // 支行名称
     String getBranchName();
 
+    void setBranchName(String branchName);
+
     HolderType getHolderType();
+
+    void setHolderType(HolderType holderType);
 
     String getHolderName();
 
+    void setHolderName(String holderName);
+
     String getNumber();
+
+    void setNumber(String number);
 
     String getLast4();
 
+    void setLast4(String last4);
+
     String getExpiryYear();
+
+    void setExpiryYear(String expiryYear);
 
     String getExpiryMonth();
 
-    Date getCreatedTime();
+    void setExpiryMonth(String expiryMonth);
+
+    Date getBoundTime();
+
+    void bind();
+
+    interface Builder extends ObjectBuilder<BankCard> {
+
+        Builder accountId(String accountId);
+
+        Builder funding(BankCardFunding funding);
+
+        Builder bankName(String bankName);
+
+        Builder branchName(String branchName);
+
+        Builder holderType(HolderType holderType);
+
+        Builder holderName(String holderName);
+
+        Builder number(String number);
+
+        Builder last4(String last4);
+
+        Builder expiryYear(String expiryYear);
+
+        Builder expiryMonth(String expiryMonth);
+
+    }
 }
