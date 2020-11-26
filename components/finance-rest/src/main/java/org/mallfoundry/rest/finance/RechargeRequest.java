370b0622acf101d448f888f2240793e2887f8d2d
TangZhi <tgioer@163.com>
2020-11-26 17:57:05 +0800

    添加 RechargeRequest 。

---
+++ components/finance-rest/src/main/java/org/mallfoundry/rest/finance/RechargeRequest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2019-2020 the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.mallfoundry.rest.finance;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.mallfoundry.finance.CurrencyCode;
+import org.mallfoundry.finance.Recharge;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class RechargeRequest {
+
+    private String accountId;
+
+    private CurrencyCode currencyCode;
+
+    private BigDecimal amount;
+
+    public Recharge assignTo(Recharge recharge) {
+        return recharge.toBuilder()
+                .accountId(this.accountId)
+                .currencyCode(this.currencyCode)
+                .amount(this.amount)
+                .build();
+    }
+}
