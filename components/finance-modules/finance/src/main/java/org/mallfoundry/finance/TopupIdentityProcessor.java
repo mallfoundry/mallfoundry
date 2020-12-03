ef73cb13488a9c83f5a22643ce26b14d2a93f624
TangZhi <tgioer@163.com>
2020-12-03 17:30:29 +0800

    使用 TransactionKeyHolder.nextKey() 获得新标识值。

--- components/finance-modules/finance/src/main/java/org/mallfoundry/finance/TopupIdentityProcessor.java
+++ components/finance-modules/finance/src/main/java/org/mallfoundry/finance/TopupIdentityProcessor.java
@@ -19,24 +19,17 @@
  package org.mallfoundry.finance;
 
  import org.apache.commons.lang3.StringUtils;
- import org.mallfoundry.keygen.PrimaryKeyHolder;
  import org.mallfoundry.security.SubjectHolder;
 
  public class TopupIdentityProcessor implements TopupProcessor {
 
-     private static final String RECHARGE_ID_VALUE_NAME = "finance.recharge.id";
-
      @Override
-     public Topup preProcessAfterCreateTopup(Topup recharge) {
-         if (StringUtils.isBlank(recharge.getId())) {
-             recharge.setId(this.nextRechargeId());
-             recharge.setOperatorId(SubjectHolder.getSubject().getId());
-             recharge.setOperator(SubjectHolder.getSubject().getNickname());
+     public Topup preProcessAfterCreateTopup(Topup topup) {
+         if (StringUtils.isBlank(topup.getId())) {
+             topup.setId(TransactionKeyHolder.nextKey(Topup.class));
+             topup.setOperatorId(SubjectHolder.getSubject().getId());
+             topup.setOperator(SubjectHolder.getSubject().getNickname());
          }
-         return recharge;
-     }
-
-     private String nextRechargeId() {
-         return "1004" + PrimaryKeyHolder.next(RECHARGE_ID_VALUE_NAME);
+         return topup;
      }
  }
