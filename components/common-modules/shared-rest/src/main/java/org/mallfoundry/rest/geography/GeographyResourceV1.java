7a689bffe88f5d0696667c46eeaa195c68f8aec0
TangZhi <tgioer@163.com>
2021-01-07 15:44:04 +0800

    添加顶级路径。

--- components/common-modules/shared-rest/src/main/java/org/mallfoundry/rest/geography/GeographyResourceV1.java
+++ components/common-modules/shared-rest/src/main/java/org/mallfoundry/rest/geography/GeographyResourceV1.java
@@ -29,7 +29,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/v1/district")
+@RequestMapping({"/v1/district", "/v1"})
 public class GeographyResourceV1 {
 
     private final GeographyService geographyService;
